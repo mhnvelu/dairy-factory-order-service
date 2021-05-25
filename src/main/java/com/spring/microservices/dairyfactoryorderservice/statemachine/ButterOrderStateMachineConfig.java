039113303eb6f42f2e8e1afa7654f6923928e5fe
Mohanavelu Subramanian <mhnvelu@gmail.com>
2021-05-25 14:10:13 +0100

    Added StateTransitions

--- src/main/java/com/spring/microservices/dairyfactoryorderservice/statemachine/ButterOrderStateMachineConfig.java
+++ src/main/java/com/spring/microservices/dairyfactoryorderservice/statemachine/ButterOrderStateMachineConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 
 import java.util.EnumSet;
 
@@ -25,6 +26,17 @@ public class ButterOrderStateMachineConfig extends StateMachineConfigurerAdapter
                 .end(ButterOrderStatusEnum.DELIVERED)
                 .end(ButterOrderStatusEnum.DELIVERY_EXCEPTION)
                 .end(ButterOrderStatusEnum.CANCELLED);
+    }
 
+    @Override
+    public void configure(StateMachineTransitionConfigurer<ButterOrderStatusEnum, ButterOrderEventEnum> transitions)
+            throws Exception {
+        transitions.withExternal().source(ButterOrderStatusEnum.NEW).target(ButterOrderStatusEnum.NEW)
+                .event(ButterOrderEventEnum.VALIDATE_ORDER)
+                // TODO add validation action here
+                .and().withExternal().source(ButterOrderStatusEnum.NEW).target(ButterOrderStatusEnum.VALIDATED)
+                .event(ButterOrderEventEnum.VALIDATION_PASSED)
+                .and().withExternal().source(ButterOrderStatusEnum.NEW).target(ButterOrderStatusEnum.VALIDATION_EXCEPTION)
+                .event(ButterOrderEventEnum.VALIDATION_FAILED);
     }
 }
